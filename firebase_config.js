e9253e4945cbfb8d1aee1228103a6e7fb1a7e05f
Kasope Johnson <kasopej@gmail.com>
2023-02-01 03:31:29 +0100

    feat: installs webpack. Specifiles project scripts as ESM modules

--- firebase_config.js
+++ firebase_config.js
@@ -1,7 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
+export {
+  ref,
+  child,
+  set,
+  update,
+  onValue,
+  remove,
+  push,
+} from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,4 +24,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const DATABASE = getDatabase(app);
