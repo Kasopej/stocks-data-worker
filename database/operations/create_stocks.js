212032cc23b9020b1758f7e2067f50229fec658b
Kasope Johnson <kasopej@gmail.com>
2023-03-01 00:31:31 +0100

    fix: refactors shares creation as function

--- database/operations/create_stocks.js
+++ database/operations/create_stocks.js
@@ -1,15 +1,19 @@
 import { DATABASE, set, ref } from "../../firebase_config.js";
 
-const commentsLocationRef = ref(DATABASE, "/stocks");
-fetch(
-  "https://api.polygon.io/v3/reference/tickers?market=stocks&exchange=XNAS&active=true",
-  {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer YtTjMC3PISJ78qtZOK7W2zGcTsNfpK9x",
-    },
-  }
-)
-  .then((res) => res.json())
-  .then(({ results }) => set(commentsLocationRef, results))
-  .catch((err) => console.log(err));
+const stocksRef = ref(DATABASE, "/stocks");
+function pushShares() {
+  fetch(
+    "https://api.polygon.io/v3/reference/tickers?market=stocks&exchange=XNAS&active=true",
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer YtTjMC3PISJ78qtZOK7W2zGcTsNfpK9x",
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then(({ results }) => set(stocksRef, results))
+    .catch((error) => {
+      throw error;
+    });
+}
