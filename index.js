2c60b9a75b882969f3fe851577b1a4f8e6200e29
Kasope Johnson <kasopej@gmail.com>
2023-03-01 00:30:34 +0100

    fix: uses async imports to load database operations modules after dependencies are imported

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-import "./firebase_config.js";
-import "./database/operations/create_stocks.js";
+import dotenv from "dotenv";
+import axios from "axios";
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+dotenv.config({ path: __dirname + "/.env" });
+global.axios = axios;
+
+import("./firebase_config.js").then((module) => {
+  module.loginUser.then(() => {
+    import("./database/operations/update_stock_trends.js");
+  });
+});
